44908ca782d9d44ede9489911d4b19614b3c629f
D-coder <chuksjoshuaa@gmail.com>
2022-05-23 02:04:26 +0100

    Update HeroBanner.js

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -28,7 +28,7 @@ const HeroBanner = ({ heroBanner }) => {
         />
 
         <div>
-         <Link href={`/product/${heroBanner.product}`}>
+         <Link href={`/product/${heroBanner.product[0]}`}>
            <a>
             <button type="button">{heroBanner.buttonText}</button>
            </a>
