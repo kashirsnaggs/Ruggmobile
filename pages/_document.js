92bcd6e93b1f4da3db05eaceeeb42fdf6acc87e8
Kashirsnaggs <snaggs.kashir@gmail.com>
2023-02-28 21:59:18 -0400

    ruggmobile

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <script
-            src={`https://www.paypal.com/sdk/js?client-id=AXiAocBCcJvq5sGlobUyKNHJO3v9bENGdpExywV2g7n7V1T7n-zxLMafi_CIrQXwapuMn6fjTpqoC3Ck&currency=USD`}></script>
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
